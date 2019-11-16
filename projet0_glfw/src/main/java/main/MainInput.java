ec3ad531193d68428b70a45d3558114792748a5a
fep <franck.en.planck@gmail.com>
2019-11-16 16:02:45 +0100

    build path/configure build path/ libraries / lwjgl-3.2.3.jar / native library location

--- projet0_glfw/src/main/java/main/MainInput.java
+++ projet0_glfw/src/main/java/main/MainInput.java
@@ -1,13 +1,19 @@
 package main;
+
+import java.io.File;
+
 /**
  * https://www.glfw.org/docs/latest/input_guide.html#joystick
  * @author fep
  *
  */
 
+
 public class MainInput {
 
 	public static void main(String[] args) {
+//		http://wiki.lwjgl.org/wiki/Distributing_Your_LWJGL_Application.html
+		System.setProperty("org.lwjgl.librarypath", new File("natives").getAbsolutePath());
 		
 		InputDetection iDetection = new InputDetection();
 		iDetection.doDetection();
