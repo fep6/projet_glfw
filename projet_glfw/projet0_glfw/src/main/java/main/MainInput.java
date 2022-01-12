5b98079b38b41df118305932b7360917ad13acd6
Msi <Msi@DESKTOP-DVL6R0Q>
2022-01-12 12:46:31 +0100

    essai push

---
+++ projet_glfw/projet0_glfw/src/main/java/main/MainInput.java
@@ -0,0 +1,21 @@
+package main;
+/**
+ * https://www.glfw.org/docs/latest/input_guide.html#joystick
+ * @author fep
+ *
+ */
+
+public class MainInput {
+
+	public static void main(String[] args) {
+		
+		InputDetection iDetection = new InputDetection();
+		iDetection.doDetection();
+		iDetection.printDetection();
+		
+		
+		// Fermeture de la bibliotheque
+		iDetection.exitGLFW();
+	}
+
+}
